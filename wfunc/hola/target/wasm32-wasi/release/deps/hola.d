e1853d583b7dbb257c1fa3afa8aa9892650aac68
Philippe Charrière <ph.charriere@gmail.com>
2022-06-05 16:15:51 +0000

    📝 save

--- wfunc/hola/target/wasm32-wasi/release/deps/hola.d
+++
@@ -1,7 +0,0 @@
-/root/runnable/target/wasm32-wasi/release/deps/hola.wasm: src/lib.rs
-
-/root/runnable/target/wasm32-wasi/release/deps/libhola.rlib: src/lib.rs
-
-/root/runnable/target/wasm32-wasi/release/deps/hola.d: src/lib.rs
-
-src/lib.rs:
--- wfunc/hola/target/wasm32-wasi/release/hola.d
+++
@@ -1,1 +0,0 @@
-/root/runnable/target/wasm32-wasi/release/hola.wasm: /root/runnable/src/lib.rs
